71a78ee00d8fb7abf71feb2c924b4d90575934dd
SvenM1234 <marcos.sven@gmail.com>
2020-05-10 10:14:41 +0200

    test2

--- routes/routeTodo.js
+++ routes/routeTodo.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const ctrlTodo = require('../controllers/ctrlTodo');
 
 
-router.get('/todo', ctrlTodo.getAll);
+// router.get('/todo', ctrlTodo.getAll);
+router.get('/todo',()=>console.log('def'));
 router.post('/todo/create', ctrlTodo.create);
 router.get('/todo/delete/:i', ctrlTodo.delete);
 
